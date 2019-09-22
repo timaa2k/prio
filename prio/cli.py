a924146198bbbfc56ab0e107e774e3263b08710a
Tim Weidner <timaa2k@gmail.com>
2019-09-22 16:09:49 +0200

    Good table

--- prio/cli.py
+++ prio/cli.py
@@ -1,7 +1,10 @@
+import datetime
+import shutil
 from typing import List
 
 import click
-from tabulate import tabulate
+import tableformatter as tf
+from colorama import Back
 
 import priolib.client
 import priolib.model
@@ -11,12 +14,58 @@ SERVER_ADDR = 'http://localhost:8080'
 
 
 def format_table(tasks: List[priolib.model.Task]) -> str:
-    header = ['ID', 'Title', 'URL', 'Age']
-    lines = []
-    for t in tasks:
-        line = [t.id, t.title, t.target, str(t.created)]
-        lines.append(line)
-    return tabulate(lines, header, tablefmt="fancy_grid")
+    rows = tasks
+
+    terminal = shutil.get_terminal_size((80, 20))
+
+    total_width = terminal.columns
+    status_width = int(0.08* total_width)
+    id_width = int(0.32* total_width)
+    title_width = int(0.15 * total_width)
+    target_width = int(0.15 * total_width)
+    age_width = int(0.20 * total_width)
+
+    cols = (
+        tf.Column(
+            'STATUS',
+            attrib='status',
+            width=status_width,
+            wrap_mode=tf.WrapMode.WRAP,
+        ),
+        tf.Column(
+            'TITLE',
+            attrib='title',
+            width=title_width,
+            wrap_mode=tf.WrapMode.WRAP,
+        ),
+        tf.Column(
+            'URL',
+            attrib='target',
+            width=target_width,
+            wrap_mode=tf.WrapMode.WRAP,
+        ),
+        tf.Column(
+            'AGE',
+            attrib='modified',
+            width=age_width,
+            wrap_mode=tf.WrapMode.WRAP,
+        ),
+        tf.Column(
+            'ID',
+            attrib='id',
+            width=id_width,
+            wrap_mode=tf.WrapMode.WRAP,
+        ),
+    )
+
+    grid = tf.AlternatingRowGrid(Back.BLACK, Back.BLACK)
+    grid = tf.SparseGrid()
+    grid.show_header = True
+    # grid.border_top = True
+    # grid.border_header_divider = True
+    # grid.col_divider = True
+    # grid.row_divider = True
+    return tf.generate_table(rows, cols, grid_style=grid)
 
 
 @click.group()
@@ -28,28 +77,50 @@ def prio():
 def next(**kwargs):
     api = priolib.client.APIClient(SERVER_ADDR)
     plan = api.get_plan()
-    tasks = plan.done + plan.today + plan.todo + plan.blocked + plan.later
+    tasks = plan.done + \
+        plan.today + \
+        plan.todo + \
+        plan.blocked + \
+        plan.later
     print(f'{str(tasks[0])}')
 
 
 @prio.command()
-def list(**kwargs):
+def now(**kwargs):
     api = priolib.client.APIClient(SERVER_ADDR)
     plan = api.get_plan()
-    print('+ Done:')
-    print(format_table(plan.done))
-    print('\n+ Today:')
-    print(format_table(plan.today))
-    print('\n+ Todo:')
-    print(format_table(plan.todo))
-    print('\n+ Blocked:')
-    print(format_table(plan.blocked))
-    print('\n+ Later:')
-    print(format_table(plan.later))
+
+    def only_status(status) -> priolib.model.Task:
+        return priolib.model.Task(id_='', status=status)
+
+    def without_status(task) -> priolib.model.Task:
+        task.status = ''
+        return task
+
+    def format_row_objects(
+        status: str,
+        tasks: List[priolib.model.Task],
+    ) -> List[priolib.model.Task]:
+        row_objects = []
+        row_objects.append(priolib.model.Task(id_='', status=''))
+        if len(tasks) == 0:
+            row_objects.append(only_status(status))
+        else:
+            row_objects.append(tasks[0])
+        return row_objects + [without_status(t) for t in tasks[1:]]
+
+    row_objects = \
+        format_row_objects(status='Done', tasks=plan.done) + \
+        format_row_objects(status='Today', tasks=plan.today) + \
+        format_row_objects(status='Todo', tasks=plan.todo) + \
+        format_row_objects(status='Blocked', tasks=plan.blocked) + \
+        format_row_objects(status='Later', tasks=plan.later)
+
+    print(format_table(row_objects))
 
 
 @prio.command()
-def all(**kwargs):
+def tasks(**kwargs):
     api = priolib.client.APIClient(SERVER_ADDR)
     tasks = api.list_tasks()
     print(format_table(tasks))
